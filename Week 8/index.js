c55685169e22d8aaaeae4d19cde302dfb83e14c1
Reem Hassan <reemhassan742r@gmail.com>
2025-05-16 22:02:38 +0300

    Add files via upload

---
+++ Week 8/index.js
@@ -0,0 +1,7 @@
+//window.alert("Hello, world!");
+/*
+document.getElementById("demo").innerHTML = "Hello, JavaScript!";
+document.getElementById("demo").style.color = "red";
+
+console.log("Hello, world!");
+*/
