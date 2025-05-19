c5e111e36997d20737538273d7bf7f141c818030
Reem Hassan <reemhassan742r@gmail.com>
2025-05-19 22:46:31 +0300

    Add files via upload

---
+++ Week 9/Js/script.js
@@ -0,0 +1,44 @@
+//Math operations + - * / ** %
+
+// + addition
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log(c); // 30
+
+// - subtraction
+let d = 30;
+let e = 10;
+let f = d - e;
+console.log(f); // 20
+
+// * multiplication
+let g = 5;
+let h = 4;
+let i = g * h;
+console.log(i); // 20
+
+// / division
+let j = 20;
+let k = 5;
+let l = j / k;
+console.log(l); // 4
+
+// ** exponentiation
+let m = 2;
+let n = 3;
+let o = m ** n;
+console.log(o); // 8
+
+// % modulus
+let p = 10;
+let q = 3;
+let r = p % q;
+console.log(r); // 1
+
+// Increment and Decrement
+let s = 5;
+s++; // s = s + 1
+console.log(s); // 6
+s--; // s = s - 1
+console.log(s); // 5
